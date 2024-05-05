83b6d468218b7adfe200f72ba189930dc865ae2c
wirawanmahardika <wiraprogrammer@gmail.com>
2024-05-05 21:16:36 +0800

    refactory: menggunakan user controller untuk memisahkannya route user

---
+++ src/controllers/users-controllers.js
@@ -0,0 +1,184 @@
+import bcrypt from "bcrypt";
+import { prisma } from "../database/prisma-client.js";
+import { error, prismaErrorResponse, success } from "../utils/response.js";
+import { mimetypeValidate } from "../utils/mimetype-validate.js";
+import { userBlobToImage } from "../utils/blobToImage.js";
+
+const signup = async (req, res) => {
+  const { password, ...data } = req.body;
+
+  const salt = await bcrypt.genSalt(12);
+  const hashPassword = await bcrypt.hash(password, salt);
+  data.password = hashPassword;
+  data.role = "user";
+
+  try {
+    const user = await prisma.users.create({
+      data: {
+        wallet: {
+          create: {
+            balance: 0,
+          },
+        },
+        ...data,
+      },
+    });
+    delete user.password;
+    delete user.id;
+    return res.status(200).json({ ...success("Berhasil signup"), data: user });
+  } catch (err) {
+    console.log(err);
+    return prismaErrorResponse(res, err);
+  }
+};
+
+const logout = (req, res) => {
+  req.logOut((err) => {
+    if (err) {
+      return res
+        .status(403)
+        .json({ ...error(403, "Tidak bisa melakukan logout") });
+    }
+
+    req.session.destroy();
+    return res.json({ ...success("Berhasil logout") });
+  });
+};
+
+const addPhoto = async (req, res) => {
+  if (!req.file) {
+    return res.status(403).json({ ...error(403, "Membutuhkan image") });
+  }
+  if (mimetypeValidate(req.file)) {
+    return res.status(403).json({ ...error(403, "Ekstensi file tidak valid") });
+  }
+
+  try {
+    await prisma.users.update({
+      where: { id: req.user.id },
+      data: { photo: req.file.buffer },
+    });
+    return res.json({ ...success("Berhasil mengubah foto profile") });
+  } catch (error) {
+    console.log(error);
+    return prismaErrorResponse(res, error);
+  }
+};
+
+const emptyPhoto = async (req, res) => {
+  try {
+    await prisma.users.update({
+      where: { id: req.user.id },
+      data: { photo: null },
+    });
+    return res.json({ ...success("Berhasil mengosongkan photo profile") });
+  } catch (err) {
+    return prismaErrorResponse(res, err);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+const getMe = async (req, res) => {
+  try {
+    const data = await prisma.users.findUnique({
+      where: { id: req.user.id },
+      select: {
+        email: true,
+        username: true,
+        fullname: true,
+        photo: true,
+        role: true,
+        emailValidated: true,
+        wallet: {
+          select: {
+            balance: true,
+          },
+        },
+      },
+    });
+    return res.json({
+      ...success("Berhasil mengambil user"),
+      data: userBlobToImage(data),
+    });
+  } catch (err) {
+    console.log(err);
+    return prismaErrorResponse(res, err);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+const editBio = async (req, res) => {
+  delete req.body.password;
+  try {
+    await prisma.users.update({
+      where: { id: req.user.id },
+      data: req.body,
+    });
+    return res.json({
+      ...success("Berhasil update data user"),
+      place: "top",
+    });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ ...error(500, "Something went wrong"), place: "top" });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  const skip = req.query.skip;
+  try {
+    const data = await prisma.users.findMany({
+      skip,
+      orderBy: {
+        role: "desc",
+      },
+      select: {
+        id: true,
+        username: true,
+        fullname: true,
+        email: true,
+        role: true,
+      },
+    });
+    return res.json({ ...success("Berhasil mengambil Semua users"), data });
+  } catch (err) {
+    console.log(err);
+    return prismaErrorResponse(res, err);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+const deleteUser = async (req, res) => {
+  if (!req.params.id) {
+    return res
+      .status(403)
+      .json({ ...error(403, "Membutuhkan kode unik user") });
+  }
+  try {
+    const [wallet, user] = await prisma.$transaction([
+      prisma.wallet.delete({ where: { id_user: req.params.id } }),
+      prisma.users.delete({ where: { id: req.params.id } }),
+    ]);
+    return res.json({ ...success("Berhasil menghapus user " + user.fullname) });
+  } catch (error) {
+    return prismaErrorResponse(res, error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export default {
+  signup,
+  logout,
+  addPhoto,
+  emptyPhoto,
+  getMe,
+  editBio,
+  getAllUsers,
+  deleteUser,
+};
--- src/routes/private/users-routes.js
+++ src/routes/private/users-routes.js
@@ -1,50 +1,18 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import { signupValidate } from "../../utils/signupValidate.js";
-import { prisma } from "../../database/prisma-client.js";
-import { error, prismaErrorResponse, success } from "../../utils/response.js";
 import passport from "passport";
 import multer from "multer";
-import { mimetypeValidate } from "../../utils/mimetype-validate.js";
+import { editBioRouteCheckKeys } from "../../middleware/itemKeysCheck.js";
+import { verifyRole } from "../../middleware/role-verify.js";
+import usersControllers from "../../controllers/users-controllers.js";
 import {
   ensureAuthenticated,
   isAuthenticated,
 } from "../../middleware/passport-middleware.js";
-import { userBlobToImage } from "../../utils/blobToImage.js";
-import { editBioRouteCheckKeys } from "../../middleware/itemKeysCheck.js";
-import { verifyRole } from "../../middleware/role-verify.js";
 
 const router = express.Router();
 
-router.post("/signup", signupValidate, async (req, res) => {
-  const { password, ...data } = req.body;
-
-  const salt = await bcrypt.genSalt(12);
-  const hashPassword = await bcrypt.hash(password, salt);
-  data.password = hashPassword;
-  data.role = "user";
-
-  try {
-    const user = await prisma.users.create({
-      data: {
-        wallet: {
-          create: {
-            balance: 0,
-          },
-        },
-        ...data,
-      },
-    });
-    delete user.password;
-    delete user.id;
-    return res.status(200).json({ ...success("Berhasil signup"), data: user });
-  } catch (err) {
-    console.log(err);
-    return prismaErrorResponse(res, err);
-  } finally {
-    await prisma.$disconnect();
-  }
-});
+router.post("/signup", signupValidate, usersControllers.signup);
 
 router.post(
   "/login",
@@ -60,156 +28,28 @@ router.post(
   }
 );
 
-router.delete("/logout", isAuthenticated, (req, res) => {
-  req.logOut((err) => {
-    if (err) {
-      return res
-        .status(403)
-        .json({ ...error(403, "Tidak bisa melakukan logout") });
-    }
-
-    req.session.destroy();
-    return res.json({ ...success("Berhasil logout") });
-  });
-});
+router.delete("/logout", isAuthenticated, usersControllers.logout);
 
 router.patch(
   "/add-photo",
   multer().single("photo"),
   isAuthenticated,
-  async (req, res) => {
-    if (!req.file) {
-      return res.status(403).json({ ...error(403, "Membutuhkan image") });
-    }
-    if (mimetypeValidate(req.file)) {
-      return res
-        .status(403)
-        .json({ ...error(403, "Ekstensi file tidak valid") });
-    }
-
-    try {
-      await prisma.users.update({
-        where: { id: req.user.id },
-        data: { photo: req.file.buffer },
-      });
-      return res.json({ ...success("Berhasil mengubah foto profile") });
-    } catch (error) {
-      console.log(error);
-      return prismaErrorResponse(res, error);
-    }
-  }
+  usersControllers.addPhoto
 );
 
-router.delete("/empty-photo", isAuthenticated, async (req, res) => {
-  try {
-    await prisma.users.update({
-      where: { id: req.user.id },
-      data: { photo: null },
-    });
-    return res.json({ ...success("Berhasil mengosongkan photo profile") });
-  } catch (err) {
-    return prismaErrorResponse(res, err);
-  } finally {
-    await prisma.$disconnect();
-  }
-});
+router.delete("/empty-photo", isAuthenticated, usersControllers.emptyPhoto);
 
-router.get("/getme", isAuthenticated, async (req, res) => {
-  try {
-    const data = await prisma.users.findUnique({
-      where: { id: req.user.id },
-      select: {
-        email: true,
-        username: true,
-        fullname: true,
-        photo: true,
-        role: true,
-        emailValidated: true,
-        wallet: {
-          select: {
-            balance: true,
-          },
-        },
-      },
-    });
-    return res.json({
-      ...success("Berhasil mengambil user"),
-      data: userBlobToImage(data),
-    });
-  } catch (err) {
-    console.log(err);
-    return prismaErrorResponse(res, err);
-  } finally {
-    await prisma.$disconnect();
-  }
-});
+router.get("/getme", isAuthenticated, usersControllers.getMe);
 
 router.put(
   "/edit-bio",
   isAuthenticated,
   editBioRouteCheckKeys,
-  async (req, res) => {
-    delete req.body.password;
-    try {
-      await prisma.users.update({
-        where: { id: req.user.id },
-        data: req.body,
-      });
-      return res.json({
-        ...success("Berhasil update data user"),
-        place: "top",
-      });
-    } catch (err) {
-      console.log(err);
-      return res
-        .status(500)
-        .json({ ...error(500, "Something went wrong"), place: "top" });
-    }
-  }
+  usersControllers.editBio
 );
 
-router.get("/", isAuthenticated, verifyRole, async (req, res) => {
-  const skip = req.query.skip;
-  try {
-    const data = await prisma.users.findMany({
-      skip,
-      orderBy: {
-        role: "desc",
-      },
-      select: {
-        id: true,
-        username: true,
-        fullname: true,
-        email: true,
-        role: true,
-      },
-    });
-    return res.json({ ...success("Berhasil mengambil Semua users"), data });
-  } catch (err) {
-    console.log(err);
-    return prismaErrorResponse(res, err);
-  } finally {
-    await prisma.$disconnect();
-  }
-});
+router.get("/", isAuthenticated, verifyRole, usersControllers.getAllUsers);
 
-router.delete("/:id", isAuthenticated, verifyRole, async (req, res) => {
-  if (!req.params.id) {
-    return res
-      .status(403)
-      .json({ ...error(403, "Membutuhkan kode unik user") });
-  }
-  try {
-    const [wallet, user] = await prisma.$transaction([
-      prisma.wallet.delete({ where: { id_user: req.params.id } }),
-      prisma.users.delete({ where: { id: req.params.id } }),
-    ]);
-    return res.json({ ...success("Berhasil menghapus user " + user.fullname) });
-  } catch (error) {
-    return prismaErrorResponse(res, error);
-  } finally {
-    await prisma.$disconnect();
-  }
-});
+router.delete("/:id", isAuthenticated, verifyRole, usersControllers.deleteUser);
 
 export default router;
